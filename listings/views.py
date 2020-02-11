03e48132d407e981946e1588f19a2132308bd193
Michael Liriano <51761089+michaelliriano@users.noreply.github.com>
2020-02-11 02:56:25 -0500

    added guest user login model and dynamic title-tags

--- listings/views.py
+++ listings/views.py
@@ -30,7 +30,17 @@ def listing(request, listing_id):
 
 
 def sold(request):
-    return render(request, 'listings/sold.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_sold=True)
+
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+
+    return render(request, 'listings/sold.html', context)
 
 
 def search(request):
@@ -70,7 +80,8 @@ def search(request):
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
-        'listings': queryset_list
+        'listings': queryset_list,
+        'values': request.GET
 
     }
     return render(request, 'listings/search.html', context)
